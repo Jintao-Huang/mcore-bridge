a01c9e0ad7df763fa1a7ea449b4a6b0fd7fb85a0
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2026-04-03 10:26:26 +0800

    fix mtp

--- src/mcore_bridge/patcher.py
+++ src/mcore_bridge/patcher.py
@@ -423,6 +423,7 @@ def _patch_mtp():
             input_ids=input_ids,
             position_ids=position_ids,
             embedding=embedding,
+            packed_seq_params=packed_seq_params,
             hidden_states=hidden_states,
         )
         assert not self.transformer_layer.self_attention.config.apply_rope_fusion
