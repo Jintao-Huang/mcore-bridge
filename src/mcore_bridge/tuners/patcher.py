d7c662ee3b5789ad4b91f7b7517404bccde74f00
Jintao <huangjintao.hjt@alibaba-inc.com>
2026-04-20 17:10:39 +0800

    compat peft 0.19 (#42)

--- src/mcore_bridge/tuners/patcher.py
+++ src/mcore_bridge/tuners/patcher.py
@@ -17,7 +17,7 @@ from .lora import LoraParallelLinear
 def dispatch_megatron(
     target: nn.Module,
     adapter_name: str,
-    lora_config,
+    lora_config=None,
     **kwargs,
 ) -> Optional[nn.Module]:
     new_module = None
